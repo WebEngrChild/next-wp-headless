263e6ed7d63d70a4c9a786179df56ca7ee96d033
joe-main <qmmplusfact@gmail.com>
2022-05-12 07:26:49 +0900

    tailwind.configのpurge設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 // tailwind.config.js
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false,
   theme: {
     extend: {},
